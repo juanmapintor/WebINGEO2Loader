d752b0be64c9b507ea34c616afaf2f5008d68a1e
Juanma Pintor <juanmapintor22@gmail.com>
2021-01-24 01:30:53 -0300

    Created tab system (to be made globally and also used in welcomeUser soon) and made login safer.

--- welcomeAdmin/welcomeAdmin.js
+++ welcomeAdmin/welcomeAdmin.js
@@ -1,10 +1,39 @@
-window.onload = () => {
+
+const checkLog = () => {
     httpRequestPromise('http://localhost/WebINGEO2Loader/php/login_state.php', null, 'POST', 'json').then((response) => {
-        if(!response.success) {
+        if(!response.success || !response.is_admin) {
             alert("No ha iniciado sesion o su sesion ha caducado.");
-            httpRequestPromise('http://localhost/WebINGEO2Loader/php/session_end.php').then(()=>{
-                location.replace("/");
-            });
+            logOut();
+        } else {
+            document.querySelector('html').setAttribute('style', 'display: flex;');
+            document.querySelector('body').setAttribute('style', 'display: flex;');
         }
     });
 };
+
+window.onload = () => {
+    checkLog();
+    configureTabs();
+};
+
+const configureTabs = () =>{
+    const selectTab = (tabs, tabNum) => {
+        let tabContents = document.querySelectorAll('.tabContent');
+        tabs.forEach(tab => {
+            tab.classList.remove('selected');
+        });
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('selected');
+        });
+
+        tabs[tabNum].classList.add('selected');
+        tabContents[tabNum].classList.add('selected');
+    };
+    let tabs = document.querySelectorAll('.tab');
+    for(let i = 0; i < tabs.length; i++){
+        tabs[i].onclick = () => {
+            selectTab(tabs, i);
+        };
+    }
+}; 
+
